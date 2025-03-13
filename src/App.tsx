322cee17d8e36b5ea0c326dc9b2a7ba225e25a52
UlisesGS <ulisesgadea28@gmail.com>
2025-03-13 10:56:33 -0300

    arrancando proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+import { AppProvider } from "./routes/AppProvider"
+import { AppRouter } from "./routes/AppRouter"
+
+const App: FC = () => {
+
+  return(
+    <AppProvider>
+      <AppRouter/>
+    </AppProvider>
+  )
+}
+
+export default App
---
+++ src/routes/AppProvider.tsx
@@ -0,0 +1,11 @@
+import { FC, ReactNode } from "react"
+
+interface AppProvdersProps {
+    children : ReactNode
+}
+
+export const AppProvider: FC<AppProvdersProps> = ({ children }) => {
+  return (
+    <div>{children}</div>
+  )
+}
